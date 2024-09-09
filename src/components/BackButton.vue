c99234ab8b7f2198a1838fc8a6139f472b0297bb
Андрей Лапаев <an.lapaev@proton-group.ru>
2024-09-09 16:37:52 +0300

    BackButton Component

---
+++ src/components/BackButton.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <section>
+      <div class="container m-auto py-6 px-6">
+        <RouterLink
+          to="/jobs"
+          class="text-green-500 hover:text-green-600 text-lg inline-flex items-center"
+        >
+          <i class="pi pi-arrow-circle-left mr-3"></i> Back to Job Listings
+        </RouterLink>
+      </div>
+    </section>
+    </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router'
+
+</script>
