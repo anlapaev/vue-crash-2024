47910ececf830ed93025f75ad580c0e83c288657
Андрей Лапаев <an.lapaev@proton-group.ru>
2024-09-09 17:17:49 +0300

    Save Job POST

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -4,34 +4,21 @@
       <div class="container m-auto max-w-2xl py-24">
         <div class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
           <form @submit.prevent="handleSubmit">
-            <h2 class="text-3xl text-center font-semibold mb-6">
-              Add Job
-            </h2>
+            <h2 class="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
             <div class="mb-4">
-              <label
-                for="type"
-                class="block text-gray-700 font-bold mb-2"
-              >Job Type</label>
+              <label for="type" class="block text-gray-700 font-bold mb-2">Job Type</label>
               <select
-                v-model="form.type"
                 id="type"
+                v-model="form.type"
                 name="type"
                 class="border rounded w-full py-2 px-3"
                 required
               >
-                <option value="Full-Time">
-                  Full-Time
-                </option>
-                <option value="Part-Time">
-                  Part-Time
-                </option>
-                <option value="Remote">
-                  Remote
-                </option>
-                <option value="Internship">
-                  Internship
-                </option>
+                <option value="Full-Time">Full-Time</option>
+                <option value="Part-Time">Part-Time</option>
+                <option value="Remote">Remote</option>
+                <option value="Internship">Internship</option>
               </select>
             </div>
 
@@ -45,13 +32,12 @@
                 class="border rounded w-full py-2 px-3 mb-2"
                 placeholder="eg. Beautiful Apartment In Miami"
                 required
-              >
+              />
             </div>
             <div class="mb-4">
-              <label
-                for="description"
-                class="block text-gray-700 font-bold mb-2"
-              >Description</label>
+              <label for="description" class="block text-gray-700 font-bold mb-2"
+                >Description</label
+              >
               <textarea
                 id="description"
                 v-model="form.description"
@@ -63,10 +49,7 @@
             </div>
 
             <div class="mb-4">
-              <label
-                for="type"
-                class="block text-gray-700 font-bold mb-2"
-              >Salary</label>
+              <label for="type" class="block text-gray-700 font-bold mb-2">Salary</label>
               <select
                 id="salary"
                 v-model="form.salary"
@@ -74,39 +57,17 @@
                 class="border rounded w-full py-2 px-3"
                 required
               >
-                <option value="Under $50K">
-                  under $50K
-                </option>
-                <option value="$50K - $60K">
-                  $50 - $60K
-                </option>
-                <option value="$60K - $70K">
-                  $60 - $70K
-                </option>
-                <option value="$70K - $80K">
-                  $70 - $80K
-                </option>
-                <option value="$80K - $90K">
-                  $80 - $90K
-                </option>
-                <option value="$90K - $100K">
-                  $90 - $100K
-                </option>
-                <option value="$100K - $125K">
-                  $100 - $125K
-                </option>
-                <option value="$125K - $150K">
-                  $125 - $150K
-                </option>
-                <option value="$150K - $175K">
-                  $150 - $175K
-                </option>
-                <option value="$175K - $200K">
-                  $175 - $200K
-                </option>
-                <option value="Over $200K">
-                  Over $200K
-                </option>
+                <option value="Under $50K">under $50K</option>
+                <option value="$50K - $60K">$50 - $60K</option>
+                <option value="$60K - $70K">$60 - $70K</option>
+                <option value="$70K - $80K">$70 - $80K</option>
+                <option value="$80K - $90K">$80 - $90K</option>
+                <option value="$90K - $100K">$90 - $100K</option>
+                <option value="$100K - $125K">$100 - $125K</option>
+                <option value="$125K - $150K">$125 - $150K</option>
+                <option value="$150K - $175K">$150 - $175K</option>
+                <option value="$175K - $200K">$175 - $200K</option>
+                <option value="Over $200K">Over $200K</option>
               </select>
             </div>
 
@@ -120,18 +81,13 @@
                 class="border rounded w-full py-2 px-3 mb-2"
                 placeholder="Company Location"
                 required
-              >
+              />
             </div>
 
-            <h3 class="text-2xl mb-5">
-              Company Info
-            </h3>
+            <h3 class="text-2xl mb-5">Company Info</h3>
 
             <div class="mb-4">
-              <label
-                for="company"
-                class="block text-gray-700 font-bold mb-2"
-              >Company Name</label>
+              <label for="company" class="block text-gray-700 font-bold mb-2">Company Name</label>
               <input
                 id="company"
                 v-model="form.company.name"
@@ -139,14 +95,13 @@
                 name="company"
                 class="border rounded w-full py-2 px-3"
                 placeholder="Company Name"
-              >
+              />
             </div>
 
             <div class="mb-4">
-              <label
-                for="company_description"
-                class="block text-gray-700 font-bold mb-2"
-              >Company Description</label>
+              <label for="company_description" class="block text-gray-700 font-bold mb-2"
+                >Company Description</label
+              >
               <textarea
                 id="company_description"
                 v-model="form.company.description"
@@ -158,10 +113,9 @@
             </div>
 
             <div class="mb-4">
-              <label
-                for="contact_email"
-                class="block text-gray-700 font-bold mb-2"
-              >Contact Email</label>
+              <label for="contact_email" class="block text-gray-700 font-bold mb-2"
+                >Contact Email</label
+              >
               <input
                 id="contact_email"
                 v-model="form.company.email"
@@ -170,13 +124,12 @@
                 class="border rounded w-full py-2 px-3"
                 placeholder="Email address for applicants"
                 required
-              >
+              />
             </div>
             <div class="mb-4">
-              <label
-                for="contact_phone"
-                class="block text-gray-700 font-bold mb-2"
-              >Contact Phone</label>
+              <label for="contact_phone" class="block text-gray-700 font-bold mb-2"
+                >Contact Phone</label
+              >
               <input
                 id="contact_phone"
                 v-model="form.company.phone"
@@ -184,7 +137,7 @@
                 name="contact_phone"
                 class="border rounded w-full py-2 px-3"
                 placeholder="Optional phone for applicants"
-              >
+              />
             </div>
 
             <div>
@@ -205,6 +158,7 @@
 <script setup>
 import { reactive } from 'vue'
 import axios from 'axios'
+import router from '@/router'
 
 const form = reactive({
   type: 'Full-Time',
@@ -221,11 +175,27 @@ const form = reactive({
 })
 
 const handleSubmit = async () => {
+  const newJob = {
+    title: form.title,
+    type: form.type,
+    description: form.description,
+    salary: form.salary,
+    location: form.location,
+    company: {
+      name: form.company.name,
+      description: form.company.description,
+      contactEmail: form.company.email,
+      contactPhone: form.company.phone
+    }
+  }
+
   try {
-    const response = await axios.post('/api/jobs', form)
-    console.log(response)
+      const response = await axios.post('/api/jobs', newJob)
+      // @todo - show toast
+      router.push(`/jobs/${response.data.id}`);
   } catch (error) {
-    console.log(error)
+      console.log(error)
+    // @todo - show toast
   }
 }
 </script>
