6b3466e8f97b35e395f02e856734953e6bbd9295
Андрей Лапаев <an.lapaev@proton-group.ru>
2024-09-09 14:13:44 +0300

    RouterLink

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,29 +1,36 @@
 <template>
-  <section class="bg-blue-50 px-4 py-10">
-    <div class="container-xl lg:container m-auto">
-      <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-        <JobListing
-          v-for="job in jobs.slice(0, props.limit || jobs.length)"
-          :key="job.id"
-          class="w-full"
-          :job="job"
-        />
+  <div>
+    <section class="bg-blue-50 px-4 py-10">
+      <div class="container-xl lg:container m-auto">
+        <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
+          Browse Jobs
+        </h2>
+        <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <JobListing
+            v-for="job in jobs.slice(0, props.limit || jobs.length)"
+            :key="job.id"
+            class="w-full"
+            :job="job"
+          />
+        </div>
       </div>
-    </div>
-  </section>
-  <section v-if="props.showButton" class="m-auto max-w-lg my-10 px-6">
-    <a
-      href="/jobs"
-      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-      >View All Jobs</a
+    </section>
+    <section
+      v-if="props.showButton"
+      class="m-auto max-w-lg my-10 px-6"
     >
-  </section>
+      <RouterLink
+        to="/jobs"
+        class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</RouterLink>
+    </section>
+  </div>
 </template>
 
 <script setup>
 import jobData from '@/jobs.json'
 import JobListing from '@/components/JobListing.vue'
+import { RouterLink } from 'vue-router'
 import { ref, defineProps } from 'vue'
 
 const jobs = ref(jobData)
